23ef52334d255c815a92452a957deffda6a1f64c
Aleksi Suuronen <aleksi.suuronen@outlook.com>
2020-09-27 19:40:15 +0300

    Weather-komponenttia muokattu

--- part2/maat/src/components/Weather.js
+++ part2/maat/src/components/Weather.js
@@ -4,8 +4,7 @@ import axios from 'axios'
 const Weather = (props) => {
 const [ weather, setWeather ] = useState([])
 //console.log(props)
-//const api_key = process.env.REACT_APP_API_KEY
-const api_key = 'ba02ae21afbe278c328f016b31784351'
+const api_key = process.env.REACT_APP_API_KEY
 const capital = props.capital
 useEffect(() => 
 axios
